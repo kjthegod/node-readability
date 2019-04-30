570649ab8cc50806dfc279cce0e9ba1410699bda
KJ <antichrist@live.com>
2019-04-29 20:02:46 -0400

    clean up
    get rid of old useless files, update useful files

--- package.sh
+++ package.sh
@@ -1,2 +1,2 @@
 NAME=node-readability
-tar -zcf ./dist/readability.tgz -C .. --exclude=".*" --exclude="test*" $NAME/lib $NAME/LICENSE.txt $NAME/README.md $NAME/package.json
+tar -zcf ./dist/readability.tgz -C .. $NAME/lib $NAME/LICENSE.txt $NAME/README.md $NAME/package.json
